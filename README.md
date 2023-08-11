9840938cb8571c6701582e811a7b618fedb710ab
Pablo Bort <pablobort98@gmail.com>
2023-08-11 07:41:08 +0200

    Best model creation

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ The data used for this project is the [HTW 2018: Wind Turbine Failures Detection
 ## Code
 
 MLflow is used to track the experiments and the models. The code is available in the `src` and `notebooks` folders of this repository.
+
+## Results
+
+Currently the benefits of using MLflow are visible in `notebooks/3-results.ipynb` where benefits/costs are calculated for each model.
+
+New models are able to save 200.965€ per year in maintenance costs, while the best models in the thesis were able to save 61.000€ per year. The percentage of total benefit of all the models with respect to maximum possible benefit that models can achieve is 83.74 %.
--- src/model_creation.py
+++ src/model_creation.py
@@ -83,170 +83,178 @@ def test_params(resample_time, center, deviation, num_dev, xgb_params) -> None:
             if id not in trb_per_failures()[comp]:
                 continue
             for col in targets[comp]:
-                MODULE_NAME = os.getenv('MODULE_NAME')
-                # Start mlflow run
-                with mlflow.start_run(run_name=f'{MODULE_NAME}_trb{id}_{col}') as run:
-                    # Set tags
-                    mlflow.set_tags({
-                        'MODULE': MODULE_NAME,
-                        'trb_num': id,
-                        'col': col,
-                        'model': 'XGBRegressor'
-                    })
 
-                    # Load train/test datasets
-                    train = pd.read_parquet(f'../data/processed/{id}/train.parquet')
+                # Load train/test datasets
+                train = pd.read_parquet(f'../data/processed/{id}/train.parquet')
 
-                    # Select the target variable
-                    y_train = train.pop(col)
+                # Select the target variable
+                y_train = train.pop(col)
 
-                    # Select the features to be used in the model
-                    train = train[features]
-                    train = create_time_columns(train)
+                # Select the features to be used in the model
+                train = train[features]
+                train = create_time_columns(train)
 
-                    # Split the train dataset into train and validation
-                    X_train, X_val, y_train, y_val = train_test_split(train, y_train, test_size=0.2, random_state=42)
+                # Split the train dataset into train and validation
+                X_train, X_val, y_train, y_val = train_test_split(train, y_train, test_size=0.2, random_state=42)
 
-                    # Scale the features
-                    scaler = StandardScaler()
-                    X_train_scaled = scaler.fit_transform(X_train)
-                    X_val_scaled = scaler.transform(X_val)
+                # Scale the features
+                scaler = StandardScaler()
+                scaler = scaler.fit(X_train)
+                X_train_scaled = scaler.transform(X_train)
+                X_val_scaled = scaler.transform(X_val)
 
-                    # Select the parameters for XGBRegressor model
-                    mlflow.log_params(xgb_params)
-                    mlflow.log_params({
-                        'resample_time': resample_time,
-                        'center': center,
-                        'deviation': deviation,
-                        'num_dev': num_dev
-                    })
+                # Train the model
+                model = XGBRegressor(**xgb_params)
+                model.fit(X_train_scaled, y_train, eval_set=[(X_val_scaled, y_val)], early_stopping_rounds=10, verbose=False)
 
-                    # Train the model
-                    model = XGBRegressor(**xgb_params)
-                    model.fit(X_train_scaled, y_train, eval_set=[(X_val_scaled, y_val)], early_stopping_rounds=10, verbose=False)
+                # Evaluate the model
+                y_pred = model.predict(X_val_scaled)
 
-                    # Save the scaler and model into mflow
-                    mlflow.sklearn.log_model(scaler, 'scaler', pyfunc_predict_fn='transform')
-                    mlflow.xgboost.log_model(model, 'model')
+                X_val = pd.DataFrame(X_val, columns=features + ['month', 'day', 'hour', 'dayofweek'])
+                X_val['y_true'] = y_val
+                X_val['y_pred'] = y_pred
+                X_val['residual'] = y_val - y_pred
 
-                    # Evaluate the model
-                    y_pred = model.predict(X_val_scaled)
-                    mlflow.log_metrics({
-                        'rmse': np.sqrt(mean_squared_error(y_val, y_pred)),
-                        'mae': mean_absolute_error(y_val, y_pred),
-                        'r2': r2_score(y_val, y_pred)
-                    })
+                threshold = {
+                    'mean': str(X_val['residual'].mean()),
+                    'median': str(X_val['residual'].median()),
+                    'std': str(X_val['residual'].std()),
+                    'mad': str((X_val['residual'] - X_val['residual'].median()).abs().median())
+                }
+                # Save json
+                with open(f'../data/interim/threshold.json', 'w') as f:
+                    json.dump(threshold, f)
 
-                    X_val = pd.DataFrame(X_val, columns=features + ['month', 'day', 'hour', 'dayofweek'])
-                    X_val['y_true'] = y_val
-                    X_val['y_pred'] = y_pred
-                    X_val['residual'] = y_val - y_pred
+                # Get costs metric
+                test = pd.read_parquet(f'../data/processed/{id}/test.parquet')
 
-                    threshold = {
-                        'mean': str(X_val['residual'].mean()),
-                        'median': str(X_val['residual'].median()),
-                        'std': str(X_val['residual'].std()),
-                        'mad': str((X_val['residual'] - X_val['residual'].median()).abs().median())
-                    }
-                    # Save json
-                    with open(f'../data/interim/threshold.json', 'w') as f:
-                        json.dump(threshold, f)
+                y_test = test.pop(col)
 
-                    mlflow.log_artifact(f'../data/interim/threshold.json')
+                # Select the features to be used in the model
+                test = test[features]
+                test = create_time_columns(test)
 
-                    # Get costs metric
-                    test = pd.read_parquet(f'../data/processed/{id}/test.parquet')
+                X_test_scaled = scaler.transform(test)
 
-                    y_test = test.pop(col)
+                test[col] = y_test
+                test['y_pred'] = model.predict(X_test_scaled)
+                test['residual'] = test[col] - test['y_pred']
 
-                    # Select the features to be used in the model
-                    test = test[features]
-                    test = create_time_columns(test)
+                test = test[[col, 'y_pred', 'residual']]
 
-                    X_test_scaled = scaler.transform(test)
+                alarms = test.copy()
+                alarms = alarms.resample(f'{resample_time}H').mean()
+                alarms['anomalous_pred'] = np.where(
+                    (alarms['residual'] >=  float(threshold[center]) + num_dev * float(threshold[deviation]))|
+                    (alarms['residual'] <=  float(threshold[center]) - num_dev * float(threshold[deviation])),
+                    1,
+                    0
+                )
+                failures = load_failures()
+                failure_dates = failures[(failures['Turbine_ID'] == id) & (failures['Component'] == comp) & (failures.index >= '2017-06-01')].index
 
-                    test[col] = y_test
-                    test['y_pred'] = model.predict(X_test_scaled)
-                    test['residual'] = test[col] - test['y_pred']
-
-                    test = test[[col, 'y_pred', 'residual']]
-
-                    alarms = test.copy()
-                    alarms = alarms.resample(f'{resample_time}H').mean()
-                    alarms['anomalous_pred'] = np.where(
-                        (alarms['residual'] >=  float(threshold[center]) + num_dev * float(threshold[deviation]))|
-                        (alarms['residual'] <=  float(threshold[center]) - num_dev * float(threshold[deviation])),
-                        1,
-                        0
-                    )
-                    failures = load_failures()
-                    failure_dates = failures[(failures['Turbine_ID'] == id) & (failures['Component'] == comp) & (failures.index > '2017-06-01')].index
-
-                    init = True
-                    for date in failure_dates:
-                        if init:
-                            alarms['anomalous_real'] = np.where(
-                                (alarms.index >= date - pd.Timedelta(days=90)) & (alarms.index <= date),
-                                1,
-                                0
-                            )
-                            init = False
-                        else:
-                            alarms['anomalous_real'] = np.where(
-                                (alarms.index >= date - pd.Timedelta(days=90)) & (alarms.index <= date),
-                                1,
-                                alarms['anomalous_real']
-                            )
-
-                    alarms[['anomalous_real', 'anomalous_pred']].plot(figsize=(15, 5), title=f'Anomalous Real vs Anomalous Predicted (turbine {id}, column {col})')
-                    for date in failure_dates:
-                        plt.axvline(x=date, color='red', linestyle='--')
-                    plt.savefig(f'../reports/check.png')
-                    plt.clf()
-                    mlflow.log_artifact(f'../reports/check.png')
-
-                    costs = load_costs()
-
-                    total_cost = 0
-                    TP_cost = 0
-                    FN_cost = 0
-                    TP = alarms[(alarms['anomalous_real']&alarms['anomalous_pred']) == 1].index
-                    if TP.shape[0] == 0:
-                        FN_cost -= costs.loc[comp, 'Replacement_Cost'] * failure_dates.shape[0]
-                        total_cost -= costs.loc[comp, 'Replacement_Cost'] * failure_dates.shape[0]
+                init = True
+                for date in failure_dates:
+                    if init:
+                        alarms['anomalous_real'] = np.where(
+                            (alarms.index >= date - pd.Timedelta(days=90)) & (alarms.index <= date),
+                            1,
+                            0
+                        )
+                        init = False
                     else:
-                        for date in failure_dates:
-                            aux = TP[(TP < date)&(TP > date - pd.Timedelta(days=90))]
-                            if len(aux) == 0:
-                                total_cost -= costs.loc[comp, 'Replacement_Cost']
-                                FN_cost -= costs.loc[comp, 'Replacement_Cost']
-                                continue
-                            days = (date - aux[0]).days + (date - aux[0]).seconds/(24*60*60)
-                            total_cost += costs.loc[comp, 'Replacement_Cost'] - (costs.loc[comp, 'Repair_Cost'] + (costs.loc[comp, 'Replacement_Cost'] - costs.loc[comp, 'Repair_Cost'])*(1 - days/90))
-                            TP_cost += costs.loc[comp, 'Replacement_Cost'] - (costs.loc[comp, 'Repair_Cost'] + (costs.loc[comp, 'Replacement_Cost'] - costs.loc[comp, 'Repair_Cost'])*(1 - days/90))
+                        alarms['anomalous_real'] = np.where(
+                            (alarms.index >= date - pd.Timedelta(days=90)) & (alarms.index <= date),
+                            1,
+                            alarms['anomalous_real']
+                        )
 
-                    FP = alarms[(alarms['anomalous_real'] == 0)&(alarms['anomalous_pred'] == 1)].index
-                    if len(FP) > 1:
-                        FP = filter_FP(FP, resample_time)
-                    FP_cost = -len(FP) * costs.loc[comp, 'Inspection_cost']
-                    total_cost += FP_cost
+                alarms[['anomalous_real', 'anomalous_pred']].plot(figsize=(15, 5), title=f'Anomalous Real vs Anomalous Predicted (turbine {id}, column {col})')
+                for date in failure_dates:
+                    plt.axvline(x=date, color='red', linestyle='--')
+                plt.switch_backend('agg')
+                plt.savefig(f'../reports/check.png')
+                plt.close('all')
 
-                    mlflow.log_metrics({
-                        'total_cost': total_cost,
-                        'TP_cost': TP_cost,
-                        'FP_cost': FP_cost,
-                        'FN_cost': FN_cost
-                    })
-                    print(f'Model: {MODULE_NAME}_trb{id}_{col}',
-                                 f'Params: {xgb_params}',  f'Resample time: {resample_time}', f'Deviation: {deviation}',
-                                 f'Total cost: {total_cost}')
+                costs = load_costs()
+
+                total_cost = 0
+                TP_cost = 0
+                FN_cost = 0
+                TP = alarms[(alarms['anomalous_real']&alarms['anomalous_pred']) == 1].index
+                if TP.shape[0] == 0:
+                    FN_cost -= costs.loc[comp, 'Replacement_Cost'] * failure_dates.shape[0]
+                    total_cost -= costs.loc[comp, 'Replacement_Cost'] * failure_dates.shape[0]
+                else:
+                    for date in failure_dates:
+                        aux = TP[(TP < date)&(TP > date - pd.Timedelta(days=90))]
+                        if len(aux) == 0:
+                            total_cost -= costs.loc[comp, 'Replacement_Cost']
+                            FN_cost -= costs.loc[comp, 'Replacement_Cost']
+                            continue
+                        days = (date - aux[0]).days + (date - aux[0]).seconds/(24*60*60)
+                        total_cost += costs.loc[comp, 'Replacement_Cost'] - (costs.loc[comp, 'Repair_Cost'] + (costs.loc[comp, 'Replacement_Cost'] - costs.loc[comp, 'Repair_Cost'])*(1 - days/90))
+                        TP_cost += costs.loc[comp, 'Replacement_Cost'] - (costs.loc[comp, 'Repair_Cost'] + (costs.loc[comp, 'Replacement_Cost'] - costs.loc[comp, 'Repair_Cost'])*(1 - days/90))
+
+                FP = alarms[(alarms['anomalous_real'] == 0)&(alarms['anomalous_pred'] == 1)].index
+                if len(FP) > 1:
+                    FP = filter_FP(FP, resample_time)
+                FP_cost = -len(FP) * costs.loc[comp, 'Inspection_cost']
+                total_cost += FP_cost
+
+                if total_cost > 0:
+                    MODULE_NAME = os.getenv('MODULE_NAME')
+                    # Start mlflow run
+                    with mlflow.start_run(run_name=f'{MODULE_NAME}_trb{id}_{col}') as run:
+                        # Set tags
+                        mlflow.set_tags({
+                            'MODULE': MODULE_NAME,
+                            'trb_num': id,
+                            'col': col,
+                            'model': 'XGBRegressor'
+                        })
+                        # Select the parameters for XGBRegressor model
+                        mlflow.log_params(xgb_params)
+                        mlflow.log_params({
+                            'resample_time': resample_time,
+                            'center': center,
+                            'deviation': deviation,
+                            'num_dev': num_dev
+                        })
+                        # Save the scaler and model into mflow
+                        mlflow.sklearn.log_model(scaler, 'scaler', pyfunc_predict_fn='transform')
+                        mlflow.xgboost.log_model(model, 'model')
+
+                        # Log metrics
+                        mlflow.log_metrics({
+                            'rmse': np.sqrt(mean_squared_error(y_val, y_pred)),
+                            'mae': mean_absolute_error(y_val, y_pred),
+                            'r2': r2_score(y_val, y_pred)
+                        })
+
+                        mlflow.log_artifact(f'../data/interim/threshold.json')
+                        mlflow.log_artifact(f'../reports/check.png')
+                    
+
+                        mlflow.log_metrics({
+                            'total_cost': total_cost,
+                            'TP_cost': TP_cost,
+                            'FP_cost': FP_cost,
+                            'FN_cost': FN_cost
+                        })
+                        print(f'Model: {MODULE_NAME}_trb{id}_{col}',
+                                        f'Params: {xgb_params}',  f'Resample time: {resample_time}', f'Deviation: {deviation}',
+                                        f'Total cost: {total_cost}')
+                else:
+                    MODULE_NAME = os.getenv('MODULE_NAME')
+                    print(f'Model: {MODULE_NAME}_trb{id}_{col}', f'Total cost: {total_cost}', 'Not saved!')
 
 if __name__ == '__main__':
-    for n_estimators in [200, 500, 1000]:
+    for n_estimators in [50, 100, 200, 500, 1000]:
         for max_depth in [3, 5, 7, 9]:
             for min_child_weight in [1, 3, 5]:
                 for learning_rate in [0.01, 0.05, 0.1]:
-                    for resample_time in [6, 12]:
+                    for resample_time in [24]:
                         for center in ['median']:
                             for deviation in ['std', 'mad']:
                                 for num_dev in [3]:                              
